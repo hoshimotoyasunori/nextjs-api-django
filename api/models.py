d9fc727c0777a22e47b89f8831b70262269fb196
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-26 14:49:58 +0900

    first commit

---
+++ api/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=500)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + " - " + self.title
+
+class Task(models.Model):
+    title = models.CharField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return str(self.id) + " - " + self.title
