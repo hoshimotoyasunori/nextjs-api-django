859c790fc214eabbf9772e3a01d124e1d048e58c
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-27 09:59:41 +0900

    edit serializers

--- api/serializers.py
+++ api/serializers.py
@@ -24,6 +24,6 @@ class TaskSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
 
     class Meta:
-        model = Post
+        model = Task
         fields = ('id', 'title', 'created_at')
 
