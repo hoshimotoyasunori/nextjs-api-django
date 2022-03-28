da3ba959d0e61aa5430c99005e61d4ea03eecbf3
hoshimotoyasunori <hoshimotoyasunori@gmail.com>
2022-03-28 12:20:14 +0900

    edit serializers.py

--- api/serializers.py
+++ api/serializers.py
@@ -9,9 +9,9 @@ class UserSerializer(serializers.ModelSerializer):
         fields = ('id', 'username', 'password')
         extra_kwargs = {'password':{'write_only':True, 'required': True}}
 
-        def create(self, validated_data):
-            user = User.objects.create_user(**validated_data)
-            return user
+    def create(self, validated_data):
+        user = User.objects.create_user(**validated_data)
+        return user
 
 class PostSerializer(serializers.ModelSerializer):
     created_at = serializers.DateTimeField(format="%Y-%m-%d %H:%M:%S", read_only=True)
